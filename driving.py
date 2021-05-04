a871aa0faabd87d8f7ad95d75d4e056993f7ee1c
Shaun Lai <vash988@yahoo.com.tw>
2021-05-04 08:24:31 +0800

    add USA

--- driving.py
+++ driving.py
@@ -3,5 +3,12 @@ age = input('請問你的年齡')
 age = int(age)
 
 if contries == '台灣':
-	age >= 18
-	print ('你可以開車喔')
+	if age >= 18:   
+	    print('你可以開車喔')
+	else:
+	    print('你還不可以開車喔')
+elif contries == '美國':
+	if age >= 16:   
+	    print('你可以開車喔')
+	else:
+	    print('你還不可以開車喔')
